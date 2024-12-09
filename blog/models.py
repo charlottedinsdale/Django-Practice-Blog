81cc449e21dd469d59f3ebcfb4677b2394ce3436
Charlotte Dinsdale <charlottedins01@gmail.com>
2024-12-09 11:53:21 +0000

    create comments model and add to admin

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
 
-class Comment(models.model):
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
