75b5ce4f52f1305843f655d76aea6ac99d475e64
Charlotte Dinsdale <charlottedins01@gmail.com>
2024-12-12 10:05:32 +0000

    create about app and wire up views, urls and template html

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,14 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
-class PostList(generic.ListView):
+
+# Class-based View
+class PostList(generic.ListView):  
     queryset = (Post.objects.filter(status=1)).order_by("-created_on")
     template_name = "blog/index.html"
     paginate_by = 6
 
+# Function-based View
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
